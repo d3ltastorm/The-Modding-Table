ba866704cdb6a3769e317b7f370e21cce4b9bd3c
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-09-04 11:09:19 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -385,12 +385,12 @@ addLayer("2layer", {
         ["display-text", function() { return getPointsDisplay() }]
     ],
 })
-addLayer("re", {
-    name: "re", // This is optional, only used in a few places, If absent it just uses the layer id
+addLayer("r", {
+    name: "r", // This is optional, only used in a few places, If absent it just uses the layer id
     symbol: "Reset", // This appears on the layer's node. Default is the id with the first letter capitalized
     symbolI18N: "Reset", // Second name of symbol for internationalization (i18n) if internationalizationMod is enabled
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -502,8 +502,8 @@ addLayer("reb", {
     name: "reb", // This is optional, only used in a few places, If absent it just uses the layer id
     symbol: "Rebirth", // This appears on the layer's node. Default is the id with the first letter capitalized
     symbolI18N: "Rebirth", // Second name of symbol for internationalization (i18n) if internationalizationMod is enabled
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -514,7 +514,7 @@ addLayer("reb", {
     resourceI18N: "rebirth points", // Second name of the resource for internationalization (i18n) if internationalizationMod is enabled
     baseResource: "reset points", // Name of resource prestige is based on
     baseResourceI18N: "reset points", // Second name of the baseResource for internationalization (i18n) if internationalizationMod is enabled
-    baseAmount() {return player.reset.points}, // Get the current amount of baseResource
+    baseAmount() {return player.r.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
 	effect() {
