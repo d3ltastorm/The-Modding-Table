b1438572f1a008f04527302dc6eb7adb5571463e
Richard Does Stuff <162537734+d3ltastorm@users.noreply.github.com>
2026-09-05 13:56:22 +0300

    Update NumberFormating.js

--- js/utils/NumberFormating.js
+++ js/utils/NumberFormating.js
@@ -171,7 +171,7 @@ function formatShitStandart(number) {
     if (number.lt(1000)) {
         s = formatWhole(number)
     } else if (number.lt(new Decimal(10).pow(3_0000_0003))) {
-        s = `${formatWhole(number.div(new Decimal(1000).pow(number.log(1000).floor())))}${shitStandart3(number.log(1000).sub(1))}`
+        s = `${formatWhole(number.div(new Decimal(1000).pow(number.log(1000).floor())))}${shitStandart(number.log(1000).sub(1))}`
     } else {
         s = shitStandart3(number.log(1000).sub(1))
     };
