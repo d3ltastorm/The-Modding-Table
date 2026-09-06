1aa6fe98b2e0418de271e85fd7d3a18720ca44e9
Richard Does Stuff <162537734+d3ltastorm@users.noreply.github.com>
2026-09-06 10:39:19 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -99,6 +99,7 @@ function getPointGen() {
 	if (hasUpgrade("r",11)) gain = gain.mul(2)
 	if (hasUpgrade("r",12)) gain = gain.mul(upgradeEffect("r", 12))
 	if (hasUpgrade("r",13)) gain = gain.mul(upgradeEffect("r", 13))
+	if (hasUpgrade("pres",14)) gain = gain.pow(1.05)
 	return gain
 }
 
