a3d0db500c23d10bb666614122b438b8dc8c45c2
Richard Does Stuff <162537734+d3ltastorm@users.noreply.github.com>
2026-09-06 13:36:44 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -93,7 +93,7 @@ addLayer("r", {
             style: {"border-radius": "0"},
 			effect() {
 				let mult = new Decimal(1);
-				mult = mult.add(player.points.add(1).log10().pow(1.5).div(4)) // (log10(x+1)^1.5)/4
+				mult = mult.add(player.points.add(1).log10().pow(1.5).div(4).mul(player.points.div(2).root(8))) // ((log10(x+1)^1.5)/4)*rt8(x/2)
 				return mult
 			}, 
             effectDisplay() { return `x${format(upgradeEffect(this.layer, this.id))}` },
